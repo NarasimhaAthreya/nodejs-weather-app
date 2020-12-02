ce0aac2e46b3104bec695d32c31c8435bf3b8f6c
Narasimha Athreya <athreya619@gmail.com>
2020-12-02 13:47:53 +0530

    Added few files

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const req = require("request")
-const url="http://api.weatherstack.com/current?access_key=7d3044abe61bbbacbcd708fb0e539667&query=mysore"
-
-req({ url: url },(error,response) => {
-
-    const data = JSON.parse(response.body)
-    console.log(data.current)
-})
+let url="http://api.weatherstack.com/current?access_key=7d3044abe61bbbacbcd708fb0e539667&query="
 
 
+// Callback function demo
+const weather = (place, callback) => {
+    console.log(url)
+    url=url+place
+    console.log(url)
 
+    req({ url: url, json:true },(error,response) => {
+        if(error) {
+            console.log("Something went wrong with request")
+        } else {
+           const output = response.body.current
+           callback(error,output)
+        }
+        })
+        
+}
 
+module.exports = weather
---
+++ main.js
@@ -0,0 +1,30 @@
+const app = require("./app.js")
+const yargs = require("yargs")
+const { demandOption } = require("yargs")
+
+yargs.command({
+
+    command: "Location",
+    description : "Enter the place to look for temperature",
+    builder:  {
+        Name : {
+            describe : "Enter the place name",
+            demandOption: true
+        }
+    },
+    handler: (argv)=> {
+        app(argv.Name,(error,name)=> {
+            if(error) {
+                console.log("Something wrong with reqest")
+            } else {
+                console.log("This is from callback function ")
+                console.log(name.temperature)
+            }
+            })
+    }
+
+
+})
+
+
+yargs.parse()
